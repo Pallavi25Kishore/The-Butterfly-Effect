8171eb1125efec1813b0bf1610f6ae7507d4936a
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-05 14:47:52 -0700

    Add front image for the tiles

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Tile = ({ tile }) => {
-  console.log(tile.src);
   return (
-    <div className="border-2 border-black">
-      <div className="front"></div>
-      <div className="back">
+    <div>
+      <div className="front" className="border-2 border-black">
+        <img src="/butterfly.png"></img>
+      </div>
+      <div className="back" className="border-2 border-black">
         <img src={tile.src}></img>
       </div>
     </div>
