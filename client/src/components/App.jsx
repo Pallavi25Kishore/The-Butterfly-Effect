bbf23b68d9ab80e558982ee618df6addaca27861
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-05 17:38:33 -0700

    add conditions to ensure that card front is visible intially when no cards are clicked

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,18 +3,18 @@ import GameBoard from "./GameBoard.jsx";
 
 const App = () => {
   const tiles = [
-    { src: "/brain-1.png" },
-    { src: "/brain-2.png" },
-    { src: "/brain-3.png" },
-    { src: "/brain-4.png" },
-    { src: "/brain-5.png" },
-    { src: "/brain-6.png" },
-    { src: "/brain-7.png" },
-    { src: "/brain-8.png" },
-    { src: "/brain-9.png" },
-    { src: "/brain-10.png" },
-    { src: "/brain-11.png" },
-    { src: "/brain-12.png" },
+    { src: "/brain-1.png", front: true },
+    { src: "/brain-2.png", front: true },
+    { src: "/brain-3.png", front: true },
+    { src: "/brain-4.png", front: true },
+    { src: "/brain-5.png", front: true },
+    { src: "/brain-6.png", front: true },
+    { src: "/brain-7.png", front: true },
+    { src: "/brain-8.png", front: true },
+    { src: "/brain-9.png", front: true },
+    { src: "/brain-10.png", front: true },
+    { src: "/brain-11.png", front: true },
+    { src: "/brain-12.png", front: true },
   ];
 
   const [currentBoard, setCurrentBoard] = useState([]);
--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const Tile = ({ tile, handleCardClick }) => {
   return (
     <div onClick={() => handleCardClick(tile.src)}>
-      <div className="front" className="border-2 border-black">
-        <img src="/butterfly.png"></img>
-      </div>
-      <div className="back" className="border-2 border-black">
-        <img src={tile.src}></img>
-      </div>
+      {tile.front ? (
+        <div className="front" className="border-2 border-black">
+          <img src="/butterfly.png"></img>
+        </div>
+      ) : (
+        <div className="back" className="border-2 border-black">
+          <img src={tile.src}></img>
+        </div>
+      )}
     </div>
   );
 };
