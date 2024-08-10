e4d2d4cfc72b417255bc4b4cfaf49827a4f04276
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-10 15:30:10 -0700

    Add the heading for the game using google fonts

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -3,9 +3,23 @@ import React from "react";
 const Head = () => {
   return (
     <div
-      className="bg-cover h-20"
-      style={{ backgroundImage: 'url("/card10.png")' }}
-    ></div>
+      className="bg-cover h-1/12 flex items-center"
+      style={{ backgroundImage: 'url("/card10.png")', width: "100vw" }}
+    >
+      <span
+        className="text-4xl ml-6 p-3"
+        style={{
+          fontFamily: '"Water Brush", cursive',
+          fontWeight: "800",
+          fontStyle: "normal",
+          background: "linear-gradient(to right, #3D3743, #907A7A)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+        }}
+      >
+        The Butterfly Effect.
+      </span>
+    </div>
   );
 };
 
