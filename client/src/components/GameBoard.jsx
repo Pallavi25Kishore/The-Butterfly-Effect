3dca3a17b1460d39eadf6d591720e573ea168f43
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-12 13:52:38 -0700

    Optimize unnecessary child component re-renders by using React.memo for the game board component

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { memo } from "react";
 import Tile from "./Tile.jsx";
 
-const GameBoard = ({ tiles, handleCardClick }) => {
+const GameBoard = memo(({ tiles, handleCardClick }) => {
   return (
-    <div className="grid grid-cols-6 gap-4">
+    <div className="grid grid-cols-6 gap-4 w-full h-full">
       {tiles.map((tile, index) => (
         <Tile key={index} tile={tile} handleCardClick={handleCardClick} />
       ))}
     </div>
   );
-};
+});
 
 export default GameBoard;
