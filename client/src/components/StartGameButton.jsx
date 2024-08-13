8dc60251b60c112ce212263681783e5ef9590780
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-12 20:22:32 -0700

    Implement start game button styling and responsiveness

--- client/src/components/StartGameButton.jsx
+++ client/src/components/StartGameButton.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const StartGameButton = ({ handleStartGame }) => {
   return (
     <div>
-      <button onClick={handleStartGame}>NEW GAME</button>
+      <button
+        onClick={handleStartGame}
+        className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-center w-48"
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/fronttile.png)",
+          fontFamily: '"Pacifico", cursive',
+          fontWeight: "800",
+          fontStyle: "normal",
+          color: "#D8D7D4",
+        }}
+      >
+        New Game
+      </button>
     </div>
   );
 };
