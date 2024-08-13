adadc992cab6dbfe9a9142028ec417c2d51510f1
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-08-12 20:23:24 -0700

    Implement step counter styling and responsiveness

--- client/src/components/StepCount.jsx
+++ client/src/components/StepCount.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const StepCount = ({ stepCount }) => {
-  return <div>{`Step Count: ${stepCount}`}</div>;
+  return (
+    <div
+      className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-center w-48"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/fronttile.png)",
+        fontFamily: '"Pacifico", cursive',
+        fontWeight: "800",
+        fontStyle: "normal",
+        color: "#D8D7D4",
+        cursor: "default",
+      }}
+    >{`Step Count: ${stepCount}`}</div>
+  );
 };
 
 export default StepCount;
